11ca1c880a348582e91ea43b778b6ddfd93701d9
Sebastian Krzyzanowicz <sebastian.krzyzanowicz@pl.abb.com>
2018-04-24 14:49:31 +0200

    Started implementation of training

--- ATP2018.SpamClassification/Classifiers/NaiveBayes.cs
+++ ATP2018.SpamClassification/Classifiers/NaiveBayes.cs
@@ -1,18 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 
 namespace ATP2018.SpamClassification.Classifiers
 {
     public class NaiveBayes : IClassifier
     {
         private readonly ITokenizer tokenizer;
-        private readonly Dictionary<SmsLabel, Group> groups;
+        private Dictionary<SmsLabel, Group> groups;
 
-        public NaiveBayes(ITokenizer tokenizer, Dictionary<SmsLabel, Group> groups)
+        public NaiveBayes(ITokenizer tokenizer)
         {
             this.tokenizer = tokenizer ?? throw new ArgumentNullException(nameof(tokenizer));
-            this.groups = groups;
+        }
+
+        public void Train(Sms[] smses, string[] classificationTokens)
+        {
+            var total = smses.Length;
+            var s = smses
+                .Select(x => new Tuple<SmsLabel, HashSet<string>>(x.Label, tokenizer.Tokenize(x.Text)))
+                .GroupBy(x => x.Item1)
+                .Select(x => new Tuple<SmsLabel, IEnumerable<string>>(x.Key, x.SelectMany(y => y.Item2)))
+                .Select(x => new Tuple<SmsLabel, Group>(x.Item1, this.Analyze(x.Item2, total, classificationTokens)));
+                
+            groups = new Dictionary<SmsLabel, Group>();
+            foreach (var ss in s)
+            {
+                groups.Add(ss.Item1,ss.Item2);
+            }
+        }
+
+        private Group Analyze(IEnumerable<string> tokens, int total, string[] classificationTokens)
+        {
+            var t = tokens.ToArray();
+
+            throw new NotImplementedException();
         }
 
         public SmsLabel Classify(string text)
